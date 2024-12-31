9069a026e2cfcffbe60ccc83d383dfdef50cd56c
Not AG7 <agg3760@gmail.com>
2024-12-31 10:25:25 +0100

    Delete Fortnite directory

--- Fortnite/Features/FireSingle.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void(*FireSingleOriginal)(AFortWeapon*);
-void FireSingle(AFortWeapon* CurrentWeapon)
-{
-	
-
-	reinterpret_cast<void* (__cdecl*)(AFortWeapon*, uintptr_t)>(CurrentWeapon, FireSingleOriginal);
-}
